688d9c871fa0fb4ec93b4ff148f84b0d462d91e2
Garett Skaar <gskaar@lso.idaho.gov>
2019-04-03 10:57:10 -0600

    Added some styles

--- pages/login.php
+++ pages/login.php
@@ -7,16 +7,19 @@ session_start();
   </head>
   <body>
     <h1>Login Page</h1>
+
     <form method="post" action="../backend/handler.php">
-      <div><label for="password">Username:</label> <input type="text" id="username" name="username"></div>
-      <div><label for="username">Password:</label> <input type="password" id="password" name="password"></div>
-      <?php
-      if (isset($_SESSION['message'])) {
-        echo "<div id='message'>" . $_SESSION['message'] . "</div>";
-      }
-      unset($_SESSION['message']);
-      ?>
-      <div><input type="submit" value="Login"></div>
+      <div class="container">
+        <div><label for="username">Username:</label> <input type="text" id="username" name="username"></div>
+        <div><label for="password">Password:</label> <input type="password" id="password" name="password"></div>
+        <?php
+        if (isset($_SESSION['message'])) {
+          echo "<div id='message'>" . $_SESSION['message'] . "</div>";
+        }
+        unset($_SESSION['message']);
+        ?>
+        <div><button type="submit" value="Login"></div>
+      </div>
     </form>
     <a href="add_employee.php">Add New Employee</a>
   </body>
