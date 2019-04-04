a5edd952e50442e25c9a41c52ed6fe8b21f723a5
Garett Skaar <gskaar@lso.idaho.gov>
2019-04-04 10:07:26 -0600

    tests

--- pages/contact_list.php
+++ pages/contact_list.php
@@ -9,13 +9,9 @@ session_start();
   <body>
     <h1>Employee Contact List</h1>
      <?php
-     echo "TEST1";
      require_once "../backend/Dao.php";
-     echo "TEST2";
      $dao = new Dao();
-     echo "TEST3";
      $employees = $dao->getEmployees();
-     echo "TEST4";
      echo "<table>";
      foreach ($employees as $employee) {
        echo "<tr><td>". $employee['employee_name'] . "</td><td>". $employee['phone_number'] . "</td><td>" . $employee['email'] . "</td></tr>";
